04d8b7800b365b017d8fa794334fbe543705698e
sofiafinlayson <finlaysonsofia@gmail.com>
2025-09-27 13:33:54 +0200

    Dogorithm files

---
+++ Dogorithm.cpp
@@ -0,0 +1,94 @@
+#include "Dogorithm.h"
+#include "Users.h"
+#include <iostream>
+#include <algorithm>
+
+/**
+ * @file Dogorithm.cpp
+ * @brief Implementation of Dogorithm concrete mediator
+ * @author [Your Name]
+ * @date [Current Date]
+ */
+
+// Static member definition
+const std::string Dogorithm::ROOM_NAME = "Dogorithm";
+
+/**
+ * @brief Constructor for Dogorithm chat room
+ */
+Dogorithm::Dogorithm() : ChatRoom() {
+    std::cout << "Dogorithm room created - Where algorithms meet adorable dogs!" << std::endl;
+}
+
+/**
+ * @brief Virtual destructor
+ */
+Dogorithm::~Dogorithm() {
+    std::cout << "Dogorithm room destroyed - The pack has disbanded!" << std::endl;
+}
+
+/**
+ * @brief Register a user with the Dogorithm chat room
+ * @param user The user to register
+ * 
+ * Adds the user to the room and notifies other users
+ */
+void Dogorithm::registerUser(User* user) {
+    if (user == nullptr) {
+        std::cerr << "Error: Cannot register null user in Dogorithm" << std::endl;
+        return;
+    }
+    
+    auto it = std::find(users.begin(), users.end(), user);
+    if (it != users.end()) {
+        std::cerr << "User " << user->getName() << " is already registered in Dogorithm" << std::endl;
+        return;
+    }
+    
+    users.push_back(user);
+    
+    std::cout << user->getName() << " has joined the pack in Dogorithm! " 
+              << "Ready to fetch some algorithms and discuss good dogs!" << std::endl;
+    
+    notifyObservers("USER_JOINED", user->getName());
+    
+    std::cout << "Dogorithm pack now has " << users.size() << " coding companions." << std::endl;
+}
+
+/**
+ * @brief Remove a user from the Dogorithm chat room
+ * @param user The user to remove
+ * 
+ * Removes the user from the room and notifies other users
+ */
+void Dogorithm::removeUser(User* user) {
+    if (user == nullptr) {
+        std::cerr << "Error: Cannot remove null user from Dogorithm" << std::endl;
+        return;
+    }
+    
+    auto it = std::find(users.begin(), users.end(), user);
+    if (it != users.end()) {
+        // Remove user from the room
+        users.erase(it);
+        
+        // Farewell message specific to Dogorithm
+        std::cout << user->getName() << " has left the Dogorithm pack. " 
+                  << "Gone to chase new coding adventures!" << std::endl;
+        
+        // Notify remaining users about the departure
+        notifyObservers("USER_LEFT", user->getName());
+        
+        std::cout << "Dogorithm pack now has " << users.size() << " coding companions." << std::endl;
+    } else {
+        std::cerr << "User " << user->getName() << " is not in Dogorithm room" << std::endl;
+    }
+}
+
+/**
+ * @brief Get the name of this chat room
+ * @return The room name "Dogorithm"
+ */
+std::string Dogorithm::getName() const {
+    return ROOM_NAME;
+}
---
+++ Dogorithm.h
@@ -0,0 +1,59 @@
+#ifndef DOGORITHM_H
+#define DOGORITHM_H
+
+#include "ChatRoom.h"
+
+/**
+ * @file Dogorithm.h
+ * @brief Dogorithm concrete mediator class - a dog-themed chat room
+ * @author [Your Name]
+ * @date [Current Date]
+ */
+
+/**
+ * @class Dogorithm
+ * @brief Concrete mediator for dog-themed discussions
+ * 
+ * Dogorithm is a concrete implementation of the ChatRoom mediator.
+ * It handles all communication between users interested in dogs and
+ * algorithm/programming topics.
+ */
+class Dogorithm : public ChatRoom {
+private:
+    static const std::string ROOM_NAME; ///< Static room name
+
+public:
+    /**
+     * @brief Constructor for Dogorithm chat room
+     */
+    Dogorithm();
+    
+    /**
+     * @brief Virtual destructor
+     */
+    virtual ~Dogorithm();
+    
+    /**
+     * @brief Register a user with the Dogorithm chat room
+     * @param user The user to register
+     * 
+     * Adds the user to the room and notifies other users
+     */
+    void registerUser(User* user);
+    
+    /**
+     * @brief Remove a user from the Dogorithm chat room
+     * @param user The user to remove
+     * 
+     * Removes the user from the room and notifies other users
+     */
+    void removeUser(User* user);
+    
+    /**
+     * @brief Get the name of this chat room
+     * @return The room name "Dogorithm"
+     */
+    std::string getName() const;
+};
+
+#endif
